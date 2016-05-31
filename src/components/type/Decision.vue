f3058de84a256bb49002f65021a35fa7ce177a5f
Thomas <info@thomasg.be>
2016-05-31 17:17:22 +0200

    Show more on click

--- src/components/type/Decision.vue
+++ src/components/type/Decision.vue
@@ -1,9 +1,9 @@
 <template>
 	<article>
-		<header>
+		<header @click="show.more=!show.more">
 			<h1 class="thing-name">{{name}}</h1>
 		</header>
-		<main>
+		<main v-if="show.more">
 			<p class="thing-description" v-if="description">{{description}}</p>
 			<div v-if="thing['dcterms:temporal']">
 			 temporal {{thing['dcterms:temporal']}}
@@ -14,15 +14,15 @@
 			<div v-if="thing['lbld:legalBackground']">
 				<h2>Juridische gronden</h2>
 				<div v-for="article in thing['lbld:legalBackground']">
-					<h4>{{a(article)['dcterms:title']}}</h4>
-					<p v-html="a(article)['dcterms:description']"></p>
+					<h4>{{art(article)['dcterms:title']}}</h4>
+					<p v-html="art(article)['dcterms:description']"></p>
 				</div>
 			</div>
 			<div v-if="thing['lbld:article']">
-				<h2>Besluit</h2>
+				<h2>Besluit</h2><pre v-text="thing|json"></pre>
 				<div v-for="article in thing['lbld:article']">
-					<h4>{{a(article)['dcterms:title']}}</h4>
-					<p v-html="a(article)['dcterms:description']"></p>
+					<h4>{{art(article)['dcterms:title']}}</h4>
+					<p v-html="art(article)['dcterms:description']"></p>
 				</div>
 			</div>
 		</main>
@@ -33,10 +33,19 @@
 import _common from './_common.js'
 
 export default {
+	data () {
+		return {
+			show: {
+				more: false
+			}
+		}
+	},
 	computed: {
-
 	},
 	methods: {
+		art (ref) {
+			return (ref && ref['@id'] && this.$parent.$parent.lookup[ref['@id']]) || ref
+		},
 		check (prop) {
 			return prop[0] !== '@' && prop !== 'schema:name' && prop !== 'schema:description' && prop !== 'dcterms:title' && prop !== 'lbld:creates' && prop !== 'lbld:gelet'
 		}
